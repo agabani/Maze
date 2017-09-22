c8d20455f48bd649383be0499b7fbca50ba6bdc0
Ahmed Agabani <agabani@live.com>
2017-09-22 19:27:04 +0100

    Refactor

--- Maze/wwwroot/js/main.js
+++ Maze/wwwroot/js/main.js
@@ -2,44 +2,59 @@
     function(THREE, Stats) {
         "use strict";
 
-        var stats = new Stats();
-
-        stats.showPanel(1);
-
-        document.body.appendChild(stats.dom);
-        console.log(status);
-
+        var container, stats;
         var camera, controls, scene, renderer;
+        var clock = new THREE.Clock();
 
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
+        var cube;
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        init();
+        animate();
 
-        document.body.appendChild(renderer.domElement);
+        function init() {
+            container = document.body;
+            camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 20000);
+            scene = new THREE.Scene();
 
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
+            renderer = new THREE.WebGLRenderer();
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(window.innerWidth, window.innerHeight);
 
-        scene.add(cube);
+            var geometry = new THREE.BoxGeometry(1, 1, 1);
+            var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            cube = new THREE.Mesh(geometry, material);
 
-        camera.position.z = 5;
+            scene.add(cube);
+
+            camera.position.z = 5;
+
+            container.appendChild(renderer.domElement);
+
+            stats = new Stats();
+            container.appendChild(stats.dom);
+
+            window.addEventListener("resize", onWindowResize, false);
+        }
+
+        function onWindowResize() {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            // controls.handleResize();
+        }
 
         function animate() {
             requestAnimationFrame(animate);
 
-            stats.begin();
-
             cube.rotation.x += 0.1;
             cube.rotation.y += 0.1;
 
-
-            renderer.render(scene, camera);
-            stats.end();
+            render();
+            stats.update();
         }
 
-        animate();
-
+        function render() {
+            // controls.update( clock.getDelta() );
+            renderer.render(scene, camera);
+        }
     });
