bb5b18af08a08ba9b11e8934d53edce74830b0a4
Ahmed Agabani <agabani@live.com>
2017-09-22 16:57:49 +0100

    requirejs

--- Maze/wwwroot/js/maze.js
+++ Maze/wwwroot/js/maze.js
@@ -1,30 +1,35 @@
-﻿(function() {
+﻿requirejs(["three"],
+    function() {
+        "use strict";
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
+        (function() {
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            var renderer = new THREE.WebGLRenderer();
 
-    document.body.appendChild(renderer.domElement);
+            renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
+            document.body.appendChild(renderer.domElement);
 
-    scene.add(cube);
+            var geometry = new THREE.BoxGeometry(1, 1, 1);
+            var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            var cube = new THREE.Mesh(geometry, material);
 
-    camera.position.z = 5;
+            scene.add(cube);
 
-    function animate() {
-        requestAnimationFrame(animate);
+            camera.position.z = 5;
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+            function animate() {
+                requestAnimationFrame(animate);
 
-        renderer.render(scene, camera);
-    }
+                cube.rotation.x += 0.1;
+                cube.rotation.y += 0.1;
 
-    animate();
+                renderer.render(scene, camera);
+            }
 
-}());
+            animate();
+
+        }());
+    });
---
+++ Maze/wwwroot/js/three.js
@@ -0,0 +1,6 @@
+﻿define("three",
+    ["/lib/three.js/build/three.js"],
+    function(THREE) {
+        window.THREE = THREE;
+        return THREE;
+    })
