f90f889419857f61211f3f916ff4748a7b5b911a
Ahmed Agabani <agabani@live.com>
2017-09-23 21:29:33 +0100

    refactor

--- Maze/wwwroot/js/app/meshfactory.js
+++ Maze/wwwroot/js/app/meshfactory.js
@@ -2,12 +2,6 @@
     ["three", "ammo"],
     function(THREE) {
         "use strict";
-        var margin = 0.05;
-
-        var wallMaterial = new THREE.MeshPhongMaterial({ color: 0x606060 });
-        var groundMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
-        var ballMaterial = new THREE.MeshPhongMaterial({ color: 0x202020 });
-        var rampMaterial = new THREE.MeshPhongMaterial({ color: 0x606060 });
 
         function Mesh(mesh, shape, body) {
             mesh.castShadow = true;
@@ -19,7 +13,7 @@
         }
 
         function PhysicsMesh(mesh, shape, mass, pos, quat) {
-            shape.setMargin(margin);
+            shape.setMargin(this.margin);
             var body = createRigidBody(mesh, shape, mass, pos, quat);
 
             Mesh.call(this, mesh, shape, body);
@@ -27,12 +21,13 @@
 
         PhysicsMesh.prototype = Object.create(Mesh.prototype);
         PhysicsMesh.constructor = PhysicsMesh;
+        PhysicsMesh.prototype.margin = 0.05;
 
         function BallMesh(pos, quat) {
             var mass = 0.7;
             var radius = 0.4;
 
-            var mesh = new THREE.Mesh(new THREE.SphereGeometry(radius, 18, 16), ballMaterial);
+            var mesh = new THREE.Mesh(new THREE.SphereGeometry(radius, 18, 16), this.material);
             var shape = new Ammo.btSphereShape(radius);
 
             PhysicsMesh.call(this, mesh, shape, mass, pos, quat);
@@ -40,11 +35,12 @@
 
         BallMesh.prototype = Object.create(PhysicsMesh.prototype);
         BallMesh.constructor = BallMesh;
+        BallMesh.prototype.material = new THREE.MeshPhongMaterial({ color: 0x202020 });
 
         function GroundMesh(options) {
             var sx = 40, sy = 1, sz = 40, mass = 0;
 
-            var mesh = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), groundMaterial);
+            var mesh = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), this.material);
             var shape = new Ammo.btBoxShape(new Ammo.btVector3(sx * 0.5, sy * 0.5, sz * 0.5));
 
             PhysicsMesh.call(this, mesh, shape, mass, options.pos, options.quat);
@@ -52,11 +48,12 @@
 
         GroundMesh.prototype = Object.create(PhysicsMesh.prototype);
         GroundMesh.constructor = GroundMesh;
+        GroundMesh.prototype.material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 
         function RampMesh(pos, quat) {
             var sx = 10, sy = 1, sz = 4, mass = 0;
 
-            var mesh = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), rampMaterial);
+            var mesh = new THREE.Mesh(new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), this.material);
             var shape = new Ammo.btBoxShape(new Ammo.btVector3(sx * 0.5, sy * 0.5, sz * 0.5));
 
             PhysicsMesh.call(this, mesh, shape, mass, pos, quat);
@@ -64,11 +61,13 @@
 
         RampMesh.prototype = Object.create(PhysicsMesh.prototype);
         RampMesh.constructor = RampMesh;
+        RampMesh.prototype.material = new THREE.MeshPhongMaterial({ color: 0x606060 });
 
         function WallMesh(options) {
             var mass = 0;
 
-            var mesh = new THREE.Mesh(new THREE.BoxGeometry(options.sx, options.sy, options.sz, 1, 1, 1), wallMaterial);
+            var mesh = new THREE.Mesh(new THREE.BoxGeometry(options.sx, options.sy, options.sz, 1, 1, 1),
+                this.material);
             var shape = new Ammo.btBoxShape(new Ammo.btVector3(options.sx * 0.5, options.sy * 0.5, options.sz * 0.5));
 
             PhysicsMesh.call(this, mesh, shape, mass, options.pos, options.quat);
@@ -76,6 +75,7 @@
 
         WallMesh.prototype = Object.create(PhysicsMesh.prototype);
         WallMesh.constructor = WallMesh;
+        WallMesh.prototype.material = new THREE.MeshPhongMaterial({ color: 0x606060 });
 
         function createRigidBody(mesh, physicsShape, mass, pos, quat) {
             mesh.position.copy(pos);
