157c6c65e939013c3302e4a86d732183702b062f
Ahmed Agabani <agabani@live.com>
2017-09-23 13:35:20 +0100

    Rendered soft bodies

--- Maze/wwwroot/js/main.js
+++ Maze/wwwroot/js/main.js
@@ -131,6 +131,10 @@
                 sphereGeometry.translate(-2, 5, 0);
                 createSoftVolume(sphereGeometry, volumeMass, 250);
 
+                var boxGeometry = new THREE.BufferGeometry().fromGeometry(new THREE.BoxGeometry(1, 1, 5, 4, 4, 20));
+                boxGeometry.translate(-2, 5, 0);
+                createSoftVolume(boxGeometry, volumeMass, 120);
+
                 // ramp
                 pos.set(3, 1, 0);
                 quat.setFromAxisAngle(new THREE.Vector3(0, 0, 1), 30 * Math.PI / 180);
@@ -150,11 +154,114 @@
             }
 
             function createSoftVolume(bufferGeometry, mass, pressure) {
-                
+                processGeometry(bufferGeometry);
+
+                var mesh = new THREE.Mesh(bufferGeometry, new THREE.MeshPhongMaterial({ color: 0xffffff }));
+                mesh.castShadow = true;
+                mesh.receiveShadow = true;
+                mesh.frustumCulled = false;
+                scene.add(mesh);
+
+                var meshSoftBody = softBodyHelpers.CreateFromTriMesh(
+                    physicsWorld.getWorldInfo(),
+                    bufferGeometry.ammoVertices,
+                    bufferGeometry.ammoIndices,
+                    bufferGeometry.ammoIndices.length / 3,
+                    true
+                );
+
+                var meshSoftBodyConfig = meshSoftBody.get_m_cfg();
+                meshSoftBodyConfig.set_viterations(40);
+                meshSoftBodyConfig.set_piterations(40);
+
+                meshSoftBodyConfig.set_collisions(0x11);
+                meshSoftBodyConfig.set_kDF(0.1);
+                meshSoftBodyConfig.set_kDP(0.01);
+                meshSoftBodyConfig.set_kPR(pressure);
+
+                meshSoftBody.get_m_materials().at(0).set_m_kLST(0.9);
+                meshSoftBody.get_m_materials().at(0).set_m_kAST(0.9);
+
+                meshSoftBody.setTotalMass(mass, false);
+                Ammo.castObject(meshSoftBody, Ammo.btCollisionObject).getCollisionShape().setMargin(margin);
+                physicsWorld.addSoftBody(meshSoftBody, 1, -1);
+                meshSoftBody.setActivationState(4);
+                softBodies.push(mesh);
             }
 
             function processGeometry(bufferGeometry) {
-                //
+                var geometry = new THREE.Geometry().fromBufferGeometry(bufferGeometry);
+                var vertsDiff = geometry.mergeVertices();
+                var indexedBufferGeometry = createIndexedBufferGeometryFromGeometry(geometry);
+                mapIndices(bufferGeometry, indexedBufferGeometry);
+            }
+
+            function createIndexedBufferGeometryFromGeometry(geometry) {
+                var numVertices = geometry.vertices.length;
+                var numFaces = geometry.faces.length;
+
+                var bufferedGeometry = new THREE.BufferGeometry();
+                var vertices = new Float32Array(numVertices * 3);
+                var indices = new (numFaces * 3 > 65535 ? Uint32Array : Uint16Array)(numFaces * 3);
+
+                for (var i = 0; i < numVertices; i++) {
+                    var p = geometry.vertices[i];
+                    var i3 = i * 3;
+                    vertices[i3] = p.x;
+                    vertices[i3 + 1] = p.y;
+                    vertices[i3 + 2] = p.z;
+                }
+
+                for (var i = 0; i < numFaces; i++) {
+                    var f = geometry.faces[i];
+                    var i3 = i * 3;
+                    indices[i3] = f.a;
+                    indices[i3 + 1] = f.b;
+                    indices[i3 + 2] = f.c;
+                }
+
+                bufferedGeometry.setIndex(new THREE.BufferAttribute(indices, 1));
+                bufferedGeometry.addAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+                return bufferedGeometry;
+            }
+
+            function mapIndices(bufferGeometry, indexedBufferGeometry) {
+                var vertices = bufferGeometry.attributes.position.array;
+                var idxVertices = indexedBufferGeometry.attributes.position.array;
+                var indices = indexedBufferGeometry.index.array;
+
+                var numIdxVertices = idxVertices.length / 3;
+                var numVertices = vertices.length / 3;
+
+                bufferGeometry.ammoVertices = idxVertices;
+                bufferGeometry.ammoIndices = indices;
+                bufferGeometry.ammoIndexAssociation = [];
+
+                for (var i = 0; i < numIdxVertices; i++) {
+                    var association = [];
+                    bufferGeometry.ammoIndexAssociation.push(association);
+
+                    var i3 = i * 3;
+
+                    for (var j = 0; j < numVertices; j++) {
+                        var j3 = j * 3;
+                        if (isEqual(idxVertices[i3],
+                            idxVertices[i3 + 1],
+                            idxVertices[i3 + 2],
+                            vertices[j3],
+                            vertices[j3 + 1],
+                            vertices[j3 + 2])) {
+                            association.push(j3);
+                        }
+
+                    }
+                }
+            }
+
+            function isEqual(x1, y1, z1, x2, y2, z2) {
+                var delta = 0.000001;
+                return Math.abs(x2 - x1) < delta && Math.abs(y2 - y1) < delta && Math.abs(z2 - z1);
             }
 
             function createParalellepiped(sx, sy, sz, mass, pos, quat, material) {
