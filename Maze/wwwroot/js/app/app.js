245e2e4666fb4ca9027b13c312e1c1813d94903c
Ahmed Agabani <agabani@live.com>
2017-09-23 18:50:20 +0100

    dropping ball

--- Maze/wwwroot/js/app/app.js
+++ Maze/wwwroot/js/app/app.js
@@ -91,19 +91,22 @@
                 pos.set(0, 0.5, 20.5);
                 new meshfactory.Wall({ pos: pos, quat: quat, sx: 40, sy: 1, sz: 1 }, physicsWorld, rigidBodies, scene);
 
-                var ballMass = 3;
-                var ballRadius = 0.4;
+                //var ballMass = 3;
+                //var ballRadius = 0.4;
 
-                var ball = new THREE.Mesh(new THREE.SphereGeometry(ballRadius, 18, 16),
-                    new THREE.MeshPhongMaterial({ color: 0x202020 }));
-                ball.castShadow = true;
-                ball.receiveShadow = true;
-                var ballShape = new Ammo.btSphereShape(ballRadius);
-                ballShape.setMargin(margin);
+                //var ball = new THREE.Mesh(new THREE.SphereGeometry(ballRadius, 18, 16),
+                //    new THREE.MeshPhongMaterial({ color: 0x202020 }));
+                //ball.castShadow = true;
+                //ball.receiveShadow = true;
+                //var ballShape = new Ammo.btSphereShape(ballRadius);
+                //ballShape.setMargin(margin);
                 pos.set(5, 10, 0);
                 quat.set(0, 0, 0, 1);
-                var ballBody = createRigidBody(ball, ballShape, ballMass, pos, quat);
-                ballBody.setFriction(0.5);
+                //var ballBody = createRigidBody(ball, ballShape, ballMass, pos, quat);
+                //ballBody.setFriction(0.5);
+
+                var mesh = new meshfactory.Ball(pos, quat, physicsWorld, rigidBodies, scene);
+
 
                 // ramp
                 pos.set(3, 1, 0);
