88fab574dbc5c33e53a7cd92a378258ffbfeeb76
Ahmed Agabani <agabani@live.com>
2017-09-23 19:22:23 +0100

    racfoc

--- Maze/wwwroot/js/app/meshfactory.js
+++ Maze/wwwroot/js/app/meshfactory.js
@@ -57,17 +57,20 @@
         }
 
         function Wall(options, physicsWorld, rigidBodies) {
-            this.mesh = createParalellepiped(options.sx,
-                options.sy,
-                options.sz,
-                0,
-                options.pos,
-                options.quat,
-                wallMaterial,
-                physicsWorld,
-                rigidBodies);
-            this.mesh.castShadow = true;
-            this.mesh.receiveShadow = true;
+            var mass = 0;
+
+            var mesh = new THREE.Mesh(new THREE.BoxGeometry(options.sx, options.sy, options.sz, 1, 1, 1), wallMaterial);
+            var shape = new Ammo.btBoxShape(new Ammo.btVector3(options.sx * 0.5, options.sy * 0.5, options.sz * 0.5));
+            shape.setMargin(margin);
+
+            var body = createRigidBody(mesh, shape, mass, options.pos, options.quat, physicsWorld, rigidBodies);
+
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
+
+            this.mesh = mesh;
+            this.shape = shape;
+            this.body = body;
         };
 
         function createParalellepiped(sx, sy, sz, mass, pos, quat, material, physicsWorld, rigidBodies) {
