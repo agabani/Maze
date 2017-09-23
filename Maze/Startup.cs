a988a39c12da4fa9775f197b8562434841f76ece
Ahmed Agabani <agabani@live.com>
2017-09-23 12:47:49 +0100

    Changed default

--- Maze/Startup.cs
+++ Maze/Startup.cs
@@ -43,7 +43,7 @@ namespace Maze
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Maze}/{action=Index}/{id?}");
             });
         }
     }
