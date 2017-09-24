05a0bdb5b1ac25e896981de6e9a91b20d24dbc61
Ahmed Agabani <agabani@live.com>
2017-09-24 23:09:50 +0100

    Added Dijkstra

---
+++ Maze.DijkstraAlgorithm/Exceptions/NoPathFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Maze.DijkstraAlgorithm.Exceptions
+{
+    public class NoPathFoundException : Exception
+    {
+        internal NoPathFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Maze.DijkstraAlgorithm/Exceptions/PathFinderException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Maze.DijkstraAlgorithm.Exceptions
+{
+    public class PathFinderException : Exception
+    {
+        internal PathFinderException(string message) : base(message)
+        {
+        }
+    }
+}
