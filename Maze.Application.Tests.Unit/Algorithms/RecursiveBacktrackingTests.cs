3673043f36342f07becedc3c1e2b56bb5be67ee6
Ahmed Agabani <agabani@live.com>
2017-09-24 18:43:00 +0100

    Theries

--- Maze.Application.Tests.Unit/Algorithms/RecursiveBacktrackingTests.cs
+++ Maze.Application.Tests.Unit/Algorithms/RecursiveBacktrackingTests.cs
@@ -5,14 +5,23 @@ namespace Maze.Application.Tests.Unit.Algorithms
 {
     public class RecursiveBacktrackingTests
     {
-        [Fact]
-        public void ItRuns()
+        [Theory]
+        [InlineData(3, 3, 0)]
+        [InlineData(3, 3, 1)]
+        [InlineData(3, 3, 2)]
+        [InlineData(5, 5, 0)]
+        [InlineData(5, 5, 1)]
+        [InlineData(5, 5, 2)]
+        [InlineData(125, 125, 0)]
+        [InlineData(125, 125, 1)]
+        [InlineData(125, 125, 2)]
+        public void ItRuns(int width, int height, int seed)
         {
             var recursiveBacktracking = new RecursiveBacktracking();
 
-            var graph = recursiveBacktracking.ProcedurallyGenerate(3, 3, 0);
+            var graph = recursiveBacktracking.ProcedurallyGenerate(width, height, seed);
 
-            Assert.Equal(9, graph.Population());
+            Assert.Equal(width * height, graph.Population());
         }
     }
 }
