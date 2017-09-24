52e75b5d05355defa6af5bf1fdf2b9cc883ef6ab
Ahmed Agabani <agabani@live.com>
2017-09-24 16:31:00 +0100

    Maze

--- Maze.Application/Services/MazeGenerator.cs
+++ Maze.Application/Services/MazeGenerator.cs
@@ -34,7 +34,46 @@ namespace Maze.Application.Services
             maze.Connect(new CartesianCoordinates(6, 4), new CartesianCoordinates(7, 4));
             maze.Connect(new CartesianCoordinates(7, 4), new CartesianCoordinates(7, 3));
 
+            maze.Connect(new CartesianCoordinates(7, 4), new CartesianCoordinates(7, 5));
 
+            maze.Connect(new CartesianCoordinates(6, 4), new CartesianCoordinates(6, 6));
+            maze.Connect(new CartesianCoordinates(6, 6), new CartesianCoordinates(7, 6));
+            maze.Connect(new CartesianCoordinates(7, 6), new CartesianCoordinates(7, 7));
+            maze.Connect(new CartesianCoordinates(7, 7), new CartesianCoordinates(3, 7));
+            maze.Connect(new CartesianCoordinates(5, 7), new CartesianCoordinates(5, 6));
+
+            maze.Connect(new CartesianCoordinates(6, 5), new CartesianCoordinates(4, 5));
+            maze.Connect(new CartesianCoordinates(4, 5), new CartesianCoordinates(4, 6));
+            maze.Connect(new CartesianCoordinates(4, 6), new CartesianCoordinates(1, 6));
+            maze.Connect(new CartesianCoordinates(1, 6), new CartesianCoordinates(1, 7));
+            maze.Connect(new CartesianCoordinates(1, 7), new CartesianCoordinates(2, 7));
+
+            maze.Connect(new CartesianCoordinates(1, 7), new CartesianCoordinates(0, 7));
+
+            maze.Connect(new CartesianCoordinates(1, 6), new CartesianCoordinates(0, 6));
+
+            maze.Connect(new CartesianCoordinates(1, 6), new CartesianCoordinates(1, 5));
+
+            maze.Connect(new CartesianCoordinates(2, 6), new CartesianCoordinates(2, 4));
+            maze.Connect(new CartesianCoordinates(2, 4), new CartesianCoordinates(0, 4));
+            maze.Connect(new CartesianCoordinates(0, 4), new CartesianCoordinates(0, 5));
+
+            maze.Connect(new CartesianCoordinates(1, 4), new CartesianCoordinates(1, 3));
+
+            maze.Connect(new CartesianCoordinates(2, 4), new CartesianCoordinates(3, 4));
+
+            maze.Connect(new CartesianCoordinates(4, 5), new CartesianCoordinates(3, 5));
+
+            maze.Connect(new CartesianCoordinates(4, 5), new CartesianCoordinates(4, 4));
+
+            maze.Connect(new CartesianCoordinates(5, 3), new CartesianCoordinates(5, 4));
+
+            maze.Connect(new CartesianCoordinates(2, 2), new CartesianCoordinates(0, 2));
+            maze.Connect(new CartesianCoordinates(0, 2), new CartesianCoordinates(0, 3));
+
+            maze.Connect(new CartesianCoordinates(0, 2), new CartesianCoordinates(0, 0));
+
+            maze.Connect(new CartesianCoordinates(1, 2), new CartesianCoordinates(1, 1));
 
             return maze.Map;
         }
