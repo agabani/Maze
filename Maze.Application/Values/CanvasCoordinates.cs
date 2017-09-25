00e8c989d8b893840f949910a02c2e5e0ee99b0f
Ahmed Agabani <agabani@live.com>
2017-09-25 18:57:48 +0100

    a

--- Maze.Application/Values/CanvasCoordinates.cs
+++ Maze.Application/Values/CanvasCoordinates.cs
@@ -13,13 +13,11 @@
 
         public static implicit operator CartesianCoordinates(CanvasCoordinates coordinates)
         {
-            //return new CartesianCoordinates(coordinates.X * 2 + 1, coordinates.Z * 2 + 1);
             return new CartesianCoordinates((coordinates.X - 1) / 2, (coordinates.Z - 1) / 2);
         }
 
         public static implicit operator CanvasCoordinates(CartesianCoordinates coordinates)
         {
-            //return new CanvasCoordinates((coordinates.X - 1) / 2, (coordinates.Z - 1) / 2);
             return new CanvasCoordinates(coordinates.X * 2 + 1, coordinates.Z * 2 + 1);
         }
     }
